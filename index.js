f5a1f59e046b09284242dc47284fc3b10fd0f718
Roman Marchen <rmarchen95@gmail.com>
2022-10-19 23:54:27 -0700

    completed graffiti assignment

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
+let header = document.querySelector('#page-header')
+header.style.textAlign = "left"
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++){
+	dogImages[i].style.borderRadius = '150px';
+    dogImages[i].style.transform = 'rotate(180deg)';
+}
+let dogNames = document.querySelectorAll('.dog-name')
+for(let i = 0; i < dogNames.length; i++){
+    dogNames[i].style.textAlign = 'left'
+}
+let footerText = document.querySelector('.footer')
+footerText.style.color = 'red'
+let generalText = document.querySelectorAll('.column')
+for(let i = 0; i < generalText.length; i++){
+    generalText[i].style.backgroundColor = 'black';
+    generalText[i].style.color = 'white'
+}
 
-// Select some elements...
+
+
+
+
+
+
